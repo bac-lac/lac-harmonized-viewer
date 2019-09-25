cfd0a07cc94a20fdbd637781c618e7b484b38f37
Gabriel Boisseau <me@boisseau.co>
2019-09-25 11:18:48 -0400

    Removing old code

--- srcts/core/errors/argument-invalid.error.ts
+++
@@ -1,8 +0,0 @@
-class ArgumentInvalidError extends Error {
-    constructor(message?: string) {
-        super(message);
-        // see: typescriptlang.org/docs/handbook/release-notes/typescript-2-2.html
-        Object.setPrototypeOf(this, new.target.prototype); // restore prototype chain
-        this.name = ArgumentInvalidError.name; // stack traces display correctly now 
-    }
-}
--- srcts/core/errors/argument-null.error.ts
+++
@@ -1,8 +0,0 @@
-class ArgumentNullError extends ArgumentInvalidError {
-    constructor(argument?: string) {
-        super(argument);
-        // see: typescriptlang.org/docs/handbook/release-notes/typescript-2-2.html
-        Object.setPrototypeOf(this, new.target.prototype); // restore prototype chain
-        this.name = ArgumentNullError.name; // stack traces display correctly now 
-    }
-}
